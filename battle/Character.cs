4b2de116f84a6830e2f897a0668318b503420b24
zubr radel <gggg26705@gmail.com>
2022-12-05 02:44:24 +0300

    Добавьте файлы проекта.

---
+++ battle/Character.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Navigation;
+
+namespace battle
+{
+    public class Character
+    {
+        protected int strength;
+        protected int dexterity;
+        protected int luck;
+        protected int constitution;
+        protected int intellect;
+        protected int lvl;
+        protected int xp;
+        protected int skillPoints;
+
+        protected double healthPoint;
+        protected double physicalAttake;
+        protected double magicalAttake;
+        protected double manaPool;
+
+        public int Exp { get => strength; set => PhysAtt = value; }
+        public int Points { get => Strenght; set => PhysAtt = value; }
+        public int Lvl { get => Strenght; set => PhysAtt = value; }
+        public double PhysAtt { get => physicalAttake; set => physicalAttake = value; }
+        public double Hp { get => healthPoint; set => healthPoint = value; }
+        public double MagAtt { get => magicalAttake; set => magicalAttake = value; }
+        public double MP { get => manaPool; set => manaPool = value; }
+        private Random Random = new Random();
+        public int Strenght { get => strength; set { strength = value; physicalAttake = value; } }
+        public int Dexterity { get => dexterity; set => dexterity = value; }
+        public int Luck { get => luck; set => luck = value; }
+        public int Constitution { get => constitution; set { Hp = value * 5; constitution = value; } }
+        public int Intelegence { get => intellect; set { MagAtt = value * 2; MP = value * 7; intellect = value;  } }
+
+
+        public string AttPart { get; set; }
+        public string BlockPart { get; set; }
+        public Character()
+        {
+            Points = 15;
+            Strenght = 1;
+            Dexterity = 1;
+            Luck = 1;
+            Constitution = 1;
+            Intelegence = 1;
+        }
+        public double IsEvasion(int EnemyDex)
+        {
+            return (Convert.ToDouble(Dexterity) - Convert.ToDouble(EnemyDex)) * 0.1;
+        }
+        public double IsCrit(int EnemyLuck)
+        {
+            return (Convert.ToDouble(Luck) - (Convert.ToDouble(EnemyLuck) * 0.5) ) * 0.1;
+        }
+        public void Heal(int mp)
+        {
+            if(Intelegence > 5)
+            {
+                if (mp < MP)
+                {
+                    Hp =+ mp* 2;
+                }
+            }
+        }
+        public string PHit(Character Enemy)
+        {
+            if(Enemy.BlockPart == AttPart)
+            {
+                if((Convert.ToDouble(Strenght) - Convert.ToDouble(Enemy.Strenght)) * 0.05 * 100 >= Random.Next(1, 100))
+                {
+                    return ("Кент блокнул(") + '\n';
+                }
+                else
+                {
+                    if (Enemy.IsEvasion(Dexterity) * 100 >= Random.Next(1, 100))
+                    {
+                        return ("Кент уклонился(") + '\n';
+                    }
+                    else
+                    {
+                        if(this.IsCrit(Enemy.Luck) *100 >= Random.Next(1, 100))
+                        {
+                            Enemy.Hp -= PhysAtt * 2;
+                            return "Критический урон!! -" + (PhysAtt * 2) + '\n' + "У врага осталось " + Enemy.Hp + '\n';
+                        }
+                        else
+                        {
+                            Enemy.Hp -= PhysAtt;
+                            return "Удар!! -" + (PhysAtt) + '\n' + "У врага осталось " + Enemy.Hp + '\n';
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (Enemy.IsEvasion(Dexterity) * 100 >= Random.Next(1, 100))
+                {
+                    return ("Кент уклонился(") + '\n';
+                }
+                else
+                {
+                    if (this.IsCrit(Enemy.Luck) * 100 >= Random.Next(1, 100))
+                    {
+                        Enemy.Hp -= PhysAtt * 2;
+                        return "Критический урон!! -" + (PhysAtt * 2) + '\n' + "У врага осталось " + Enemy.Hp + '\n';
+                    }
+                    else
+                    {
+                        Enemy.Hp -= PhysAtt;
+                        return "Удар!! -" + (PhysAtt) + '\n' + "У врага осталось " + Enemy.Hp + '\n';
+                    }
+                }
+            }
+            return "";
+            
+        }
+        public string MHit(Character Enemy)
+        {
+            if(MP -5 > 0)
+            {
+                MP -= 5;
+                if (Enemy.BlockPart == AttPart)
+                {
+                    if ((Convert.ToDouble(Strenght) - Convert.ToDouble(Enemy.Strenght)) * 0.05 * 100 >= Random.Next(1, 100))
+                    {
+                        return ("Кент блокнул(") + '\n';
+                    }
+                    else
+                    {
+                        if (Enemy.IsEvasion(Dexterity) * 100 >= Random.Next(1, 100))
+                        {
+                            return ("Кент уклонился(") + '\n';
+                        }
+                        else
+                        {
+                            if (this.IsCrit(Enemy.Luck) * 100 >= Random.Next(1, 100))
+                            {
+                                Enemy.Hp -= MagAtt * 2;
+                                return "Критический урон!! -" + (MagAtt * 2) + '\n' + "У врага осталось " + Enemy.Hp + '\n';
+                            }
+                            else
+                            {
+                                Enemy.Hp -= MagAtt;
+                                return "Удар!! -" + (MagAtt) + '\n' + "У врага осталось " + Enemy.Hp + '\n';
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    if (Enemy.IsEvasion(Dexterity) * 100 >= Random.Next(1, 100))
+                    {
+                        return ("Кент уклонился(") + '\n';
+                    }
+                    else
+                    {
+                        if (this.IsCrit(Enemy.Luck) * 100 >= Random.Next(1, 100))
+                        {
+                            Enemy.Hp -= MagAtt * 2;
+                            return "Критический урон!! -" + (MagAtt * 2) + '\n' + "У врага осталось " + Enemy.Hp + '\n';
+                        }
+                        else
+                        {
+                            Enemy.Hp -= MagAtt;
+                            return "Удар!! -" + (MagAtt) + '\n' + "У врага осталось " + Enemy.Hp + '\n';
+                        }
+                    }
+                }
+            }
+            else
+            {
+                return "Мало маны(" + '\n';
+            }
+            return "";
+        }
+        public void ExpUp(int exp)
+        {
+            switch (Lvl)
+            {
+                case (1):
+                    if(100 < Exp + exp)
+                    {
+                        Lvl++;
+                        Exp = Exp + exp - 100;
+                        Points += 2;
+                    }
+                    else
+                    {
+                        Exp += exp;
+                    }
+                    break;
+                case (2):
+                    if (300 < Exp + exp)
+                    {
+                        Lvl++;
+                        Exp = Exp + exp - 300;
+                        Points += 2;
+                    }
+                    else
+                    {
+                        Exp += exp;
+                    }
+                    break;
+                case (3):
+                    if (800 < Exp + exp)
+                    {
+                        Lvl++;
+                        Exp = Exp + exp - 800;
+                        Points += 3;
+                    }
+                    else
+                    {
+                        Exp += exp;
+                    }
+                    break;
+                case (4):
+                    if (1800 < Exp + exp)
+                    {
+                        Lvl++;
+                        Exp = Exp + exp - 1800;
+                        Points += 2;
+                    }
+                    else
+                    {
+                        Exp += exp;
+                    }
+                    break;
+                case (5):
+                    if (3000 < Exp + exp)
+                    {
+                        Lvl++;
+                        Exp = Exp + exp - 3000;
+                        Points += 5;
+                    }
+                    else
+                    {
+                        Exp += exp;
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ battle/MainWindow.xaml.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace battle
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Character FirstPalyer;
+        Character SecondPalyer;
+        bool isFight;
+        bool MPressed;
+        bool LPressed;
+        bool PPressed;
+        bool ZPressed;
+        bool APressed;
+        bool QPressed;
+        public MainWindow()
+        {
+            InitializeComponent();
+             MPressed = false;
+             LPressed = false;
+            PPressed = false;
+            ZPressed = false;
+            APressed = false;
+            QPressed = false;
+        }
+
+        private void BeginFight_Click(object sender, RoutedEventArgs e)
+        {
+            isFight = true;
+            FirstPalyer = new Character();
+            SecondPalyer = new Character();
+            FightInfo.Text += "Бой начался" + '\n';
+            /*            while ((FirstPalyer.Hp > 0 || SecondPalyer.Hp > 0) && isFight)
+                        {
+                            if(FightInfo.Text.Split('\n').Length > 10)
+                            {
+                                FightInfo.Text = "";
+                            }
+                        }*/
+        }
+
+        async private void EndFight_Click(object sender, RoutedEventArgs e)
+        {
+            isFight = false;
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(isFight)
+            {
+                if (FightInfo.Text.Split('\n').Length > 10)
+                {
+                    FightInfo.Text = "";
+                }
+                if (e.Key == Key.M)//Маг урон 2 игрока
+                {
+                    MPressed = true;
+                }
+                else if (e.Key == Key.P)//Физ урон 2 игрока
+                {
+                    PPressed = true;
+                }
+                else if (e.Key == Key.L)//Блок 2 игрока
+                {
+                    LPressed = true;
+                }
+                if (MPressed)
+                {
+                    if (e.Key == Key.N) //Ноги
+                    {
+                        SecondPalyer.AttPart = "Ноги";
+                        FightInfo.Text += "Второй игрок атакует" + '\n' + SecondPalyer.MHit(FirstPalyer);
+                    }
+                    else if (e.Key == Key.J) //Тело
+                    {
+                        SecondPalyer.AttPart = "Тело";
+                        FightInfo.Text += "Второй игрок атакует" + '\n' + SecondPalyer.MHit(FirstPalyer);
+                    }
+                    else if (e.Key == Key.K) //Руки
+                    {
+                        SecondPalyer.AttPart = "Руки";
+                        FightInfo.Text += "Второй игрок атакует" + '\n' + SecondPalyer.MHit(FirstPalyer);
+                    }
+                    else if (e.Key == Key.U) //Голова
+                    {
+                        SecondPalyer.AttPart = "Голова";
+                        FightInfo.Text += "Второй игрок атакует" + '\n' + SecondPalyer.MHit(FirstPalyer);
+                    }
+                }
+                else if (PPressed)//Физ урон 2 игрока
+                {
+                    if (e.Key == Key.N) //Ноги
+                    {
+                        SecondPalyer.AttPart = "Ноги";
+                        FightInfo.Text += "Второй игрок атакует" + '\n' + SecondPalyer.PHit(FirstPalyer);
+                    }
+                    else if (e.Key == Key.J) //Тело
+                    {
+                        SecondPalyer.AttPart = "Тело";
+                        FightInfo.Text += "Второй игрок атакует" + '\n' + SecondPalyer.PHit(FirstPalyer);
+                    }
+                    else if (e.Key == Key.K) //Руки
+                    {
+                        SecondPalyer.AttPart = "Руки";
+                        FightInfo.Text += "Второй игрок атакует" + '\n' + SecondPalyer.PHit(FirstPalyer);
+
+                    }
+                    else if (e.Key == Key.U) //Голова
+                    {
+                        SecondPalyer.AttPart = "Голова";
+                        FightInfo.Text += "Второй игрок атакует" + '\n' + SecondPalyer.PHit(FirstPalyer);
+
+                    }
+
+                }
+                else if (LPressed)//Блок 2 игрока
+                {
+                    if (e.Key == Key.N) //Ноги
+                    {
+                        SecondPalyer.BlockPart = "Ноги";
+                    }
+                    else if (e.Key == Key.J) //Тело
+                    {
+                        SecondPalyer.BlockPart = "Тело";
+                    }
+                    else if (e.Key == Key.K) //Руки
+                    {
+                        SecondPalyer.BlockPart = "Руки";
+                    }
+                    else if (e.Key == Key.U) //Голова
+                    {
+                        SecondPalyer.BlockPart = "Голова";
+                    }
+                }
+                /////////
+                if (e.Key == Key.Z)//Маг урон 1 игрока
+                {
+                    ZPressed = true;
+                }
+                else if (e.Key == Key.A)//Физ урон 1 игрока
+                {
+                    APressed = true;
+                }
+                else if (e.Key == Key.Q)//Блок 1 игрока
+                {
+                    QPressed = true;
+
+                }
+                if (ZPressed)//Маг урон 1 игрока
+                {
+                    if (e.Key == Key.D) //Ноги
+                    {
+                        FirstPalyer.AttPart = "Ноги";
+                        FightInfo.Text += "Первый игрок атакует" + '\n' + FirstPalyer.MHit(SecondPalyer);
+                    }
+                    else if (e.Key == Key.X) //Тело
+                    {
+                        FirstPalyer.AttPart = "Ноги";
+                        FightInfo.Text += "Первый игрок атакует" + '\n' + FirstPalyer.MHit(SecondPalyer);
+                    }
+                    else if (e.Key == Key.F) //Руки
+                    {
+                        FirstPalyer.AttPart = "Ноги";
+                        FightInfo.Text += "Первый игрок атакует" + '\n' + FirstPalyer.MHit(SecondPalyer);
+                    }
+                    else if (e.Key == Key.V) //Голова
+                    {
+                        FirstPalyer.AttPart = "Ноги";
+                        FightInfo.Text += "Первый игрок атакует" + '\n' + FirstPalyer.MHit(SecondPalyer);
+                    }
+
+                }
+                else if (APressed)//Физ урон 1 игрока
+                {
+                    if (e.Key == Key.D) //Ноги
+                    {
+                        FirstPalyer.AttPart = "Ноги";
+                        FightInfo.Text += "Первый игрок атакует" + '\n' + FirstPalyer.PHit(SecondPalyer);
+                    }
+                    else if (e.Key == Key.X) //Тело
+                    {
+                        FirstPalyer.AttPart = "Ноги";
+                        FightInfo.Text += "Первый игрок атакует" + '\n' + FirstPalyer.PHit(SecondPalyer);
+                    }
+                    else if (e.Key == Key.F) //Руки
+                    {
+                        FirstPalyer.AttPart = "Ноги";
+                        FightInfo.Text += "Первый игрок атакует" + '\n' + FirstPalyer.PHit(SecondPalyer);
+                    }
+                    else if (e.Key == Key.V) //Голова
+                    {
+                        FirstPalyer.AttPart = "Ноги";
+                        FightInfo.Text += "Первый игрок атакует" + '\n' + FirstPalyer.PHit(SecondPalyer);
+                    }
+
+                }
+                else if (QPressed)//Блок 1 игрока
+                {
+                    if (e.Key == Key.D) //Ноги
+                    {
+                        FirstPalyer.AttPart = "Ноги";
+                    }
+                    else if (e.Key == Key.X) //Тело
+                    {
+                        FirstPalyer.AttPart = "Ноги";
+                    }
+                    else if (e.Key == Key.F) //Руки
+                    {
+                        FirstPalyer.AttPart = "Ноги";
+                    }
+                    else if (e.Key == Key.V) //Голова
+                    {
+                        FirstPalyer.AttPart = "Ноги";
+                    }
+
+                }
+                if (FirstPalyer.Hp <= 0)
+                {
+                    FightInfo.Text += "Выйграл Первый игрок";
+
+                }
+                else if (SecondPalyer.Hp <= 0)
+                {
+                    FightInfo.Text += "Выйграл Второй игрок";
+                }
+            }
+            else
+            {
+                if(FirstPalyer.Hp < SecondPalyer.Hp)
+                {
+                    FirstPalyer.ExpUp(SecondPalyer.Hp);
+                }
+                FightInfo.Text = "Бой не идёт";
+            }
+            
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.M)//Маг урон 2 игрока
+            {
+                MPressed = false;
+            }
+            else if (e.Key == Key.P)//Физ урон 2 игрока
+            {
+                PPressed = false;
+            }
+            else if (e.Key == Key.L)//Блок 2 игрока
+            {
+                LPressed = false;
+            }
+            if (e.Key == Key.Z)//Маг урон 1 игрока
+            {
+                ZPressed = false;
+            }
+            else if (e.Key == Key.A)//Физ урон 1 игрока
+            {
+                APressed = false;
+            }
+            else if (e.Key == Key.Q)//Блок 1 игрока
+            {
+                QPressed = false;
+
+            }
+        }
+    }
+}
